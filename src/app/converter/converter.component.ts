ea26662df67d19faf678a2a008f943869187cfb4
Alex <a1980dominov@gmail.com>
2022-09-26 12:37:37 +0300

    I`m writing a few functions.

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -16,15 +16,20 @@ export class ConverterComponent implements OnInit {
   par: Part[] = []
   num: number = 1
   item: number = 0;
-  cangetMoney: string = 'USD'
-  cangetMoneyTo: string = 'UAH'
+  changetMoney: string = 'USD'
+  changetMoneyTo: string = 'UAH'
   response: string = '1'
   responseNumber: number = 1
   itemjson: any = []
   responseTwo: string = '1'
-  responseNumberTwo: number = 1
   it:any
 
+  itemInputValue:string=''
+  itemInputValueTwo:string=''
+  itemInputValueNumberTwo:number=0
+  selectResponseOne:number=0
+  multipleResponseOne:number=0
+  multipleResponseTwo:number=0
   constructor(private serv: CyrrencyService) {
   }
 
@@ -35,14 +40,14 @@ export class ConverterComponent implements OnInit {
 
   }
 
-  canget(money: string) {
-    this.cangetMoney = money
-    console.log(this.cangetMoney)
+  changet(money: string) {
+    this.changetMoney = money
+    console.log(this.changetMoney)
   }
 
-  cangetTo(moneyTo: string) {
-    this.cangetMoneyTo = moneyTo
-    console.log(this.cangetMoneyTo)
+  changetTo(moneyTo: string) {
+    this.changetMoneyTo = moneyTo
+    console.log(this.changetMoneyTo)
   }
 
 
@@ -52,62 +57,71 @@ export class ConverterComponent implements OnInit {
 
 
 
-  multiple() {
+  multipleInputTwo(){
     this.responseNumber = Number(this.response)
-    this.responseNumberTwo = Number(this.responseTwo)
-    return this.responseNumberTwo * this.responseNumber
+    this.itemInputValueNumberTwo = Number(this.itemInputValue)
+    return this.multipleResponseTwo= this.itemInputValueNumberTwo * this.responseNumber
 
   }
-  multipleTwo() {
-    this.responseNumber = Number(this.response)
-    this.responseNumberTwo = Number(this.responseTwo)
-    return this.responseNumber*this.responseNumberTwo
+   multipleInputOne(){
 
-  }
+     this.selectResponseOne=Number(this.responseTwo)
+     this.itemInputValueNumberTwo=Number(this.itemInputValueTwo)
+     return this.multipleResponseOne= this.selectResponseOne*this.itemInputValueNumberTwo
+   }
 
 reset(){
-    return this.response=''
+    return this.itemInputValue='' , this.itemInputValueTwo=''
 }
   convertOne() {
-    console.log(this.cangetMoneyTo)
-    this.serv.getCurrency(this.cangetMoneyTo).subscribe(data => {
+    console.log(this.changetMoneyTo)
+    this.serv.getCurrency(this.changetMoneyTo).subscribe(data => {
       this.itemjson = JSON.stringify(data);
       this.itemjson = JSON.parse(this.itemjson)
       console.log(this.itemjson)
-      if (this.cangetMoney == 'UAH') {
+      if (this.changetMoney == 'UAH') {
         this.responseTwo = this.itemjson.rates.UAH
       }
-      if (this.cangetMoney == 'USD') {
+      if (this.changetMoney == 'USD') {
         this.responseTwo = this.itemjson.rates.USD
       }
-      if (this.cangetMoney == 'EUR') {
+      if (this.changetMoney == 'EUR') {
         this.responseTwo = this.itemjson.rates.EUR
       }
-      if (this.cangetMoney == 'ALL') {
+      if (this.changetMoney == 'ALL') {
+        this.responseTwo = this.itemjson.rates.ALL
+      }
+      if (this.changetMoney == 'BRL') {
         this.responseTwo = this.itemjson.rates.ALL
       }
     })
+    this.multipleInputOne()
   }
 
+
   convertTwo() {
-    console.log(this.cangetMoney)
-    this.serv.getCurrency(this.cangetMoney).subscribe(data => {
+    console.log(this.changetMoney)
+    this.serv.getCurrency(this.changetMoney).subscribe(data => {
       this.itemjson = JSON.stringify(data);
       this.itemjson = JSON.parse(this.itemjson)
       console.log(this.itemjson)
-      if (this.cangetMoneyTo == 'UAH') {
+      if (this.changetMoneyTo == 'UAH') {
         this.response = this.itemjson.rates.UAH
       }
-      if (this.cangetMoneyTo == 'USD') {
+      if (this.changetMoneyTo == 'USD') {
         this.response = this.itemjson.rates.USD
       }
-      if (this.cangetMoneyTo == 'EUR') {
+      if (this.changetMoneyTo == 'EUR') {
         this.response = this.itemjson.rates.EUR
       }
-      if (this.cangetMoney == 'ALL') {
-        this.responseTwo = this.itemjson.rates.ALL
+      if (this.changetMoneyTo == 'ALL') {
+        this.response = this.itemjson.rates.ALL
+      }
+      if (this.changetMoneyTo == 'BRL') {
+        this.response = this.itemjson.rates.ALL
       }
     })
+    this.multipleInputTwo()
   }
 
 
